9f4cb8ce0064089ee453ce504bb760b12c30d12f
Lida Aupova <lida.aupova@gmail.com>
2023-03-10 20:39:22 +0300

    Изменены данные по получению случайных персонажей

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,7 +4,7 @@ class MarvelService {
 
     getResource = async (url) => {
         let res = await fetch(url);
-        
+
         if(!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
@@ -12,12 +12,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: !char.description ? "Unfortunately there is no description for this character" : char.description.slice(0, 230),
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
