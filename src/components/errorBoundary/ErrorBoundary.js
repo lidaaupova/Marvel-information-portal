041cfca3a796b3062cff0cdebaa58b26662c211b
Lida Aupova <lida.aupova@gmail.com>
2023-03-13 12:47:25 +0300

    Работа с предохранителями, отлавливаем ошибки при помощи componentDidCatch

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class ErrorBoundary extends Component {
+    state = {
+        error: false
+    }
+
+    // Метод Предохрнаителя, который только обновляет state ошибки!
+    // static getDerivedStateFromError(error) {
+    //     return {error: true};
+    // }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
